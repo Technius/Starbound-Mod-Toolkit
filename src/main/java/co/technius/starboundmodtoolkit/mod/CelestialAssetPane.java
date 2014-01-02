6dc3704b8edeceddf198c22d7aa28986ad0eb00a
Technius <techniux@gmail.com>
2014-01-02 11:41:39 -0800

    Oops, forgot to include CelestialAssetPane

---
+++ src/main/java/co/technius/starboundmodtoolkit/mod/CelestialAssetPane.java
@@ -0,0 +1,29 @@
+package co.technius.starboundmodtoolkit.mod;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import co.technius.starboundmodtoolkit.mod.JsonObjectBinding.Type;
+
+public class CelestialAssetPane extends ItemAssetPane
+{
+	@JsonObjectBinding(key = "image", type = Type.STRING, required = true)
+	TextField image = AssetPaneUtils.noEmptyTextField(enable);
+	
+	@JsonObjectBinding(key = "sectorUnlock", type = Type.STRING, required = true)
+	TextField sectorUnlock = AssetPaneUtils.noEmptyTextField(enable);
+	
+	@JsonObjectBinding(key = "tierRecipesUnlock", type = Type.STRING, required = true)
+	TextField tierRecipesUnlock = AssetPaneUtils.noEmptyTextField(enable);
+	
+	@JsonObjectBinding(key = "unlockMessage", type = Type.STRING, required = true)
+	TextField unlockMessage = AssetPaneUtils.noEmptyTextField(enable);
+	
+	public CelestialAssetPane(JsonAsset asset) 
+	{
+		super(asset);
+		form.add("Image", image, new Label("Required"));
+		form.add("Unlocked Sector", sectorUnlock, new Label("Required"));
+		form.add("Unlocked Recipe Tier", tierRecipesUnlock, new Label("Required"));
+		form.add("Message", unlockMessage, new Label("Required"));
+	}
+}
